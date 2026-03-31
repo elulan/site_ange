43e4660e0d169000cd35e80a6779775a9c109897
elulan <elfo.dreamland@outlook.com>
2026-03-31 22:39:03 +0200

    correction 3sec diapo

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,7 +45,7 @@ document.addEventListener('alpine:init', () => {
 
             this.interval = setInterval(() => {
                 this.current = (this.current + 1) % this.images.length;
-            }, 7000);
+            }, 3000);
         }
     }));
 });
